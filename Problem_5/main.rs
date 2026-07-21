a052f39f8553e8562d227ebbe269581a3fa738b1
Felix <cuong.m9325@gmail.com>
2026-07-22 02:50:48 +1000

    problem 6

---
+++ Problem_5/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+	//since the range is only 20,
+	//I'm not designing a whole algorithm around it?
+	//primes under 20: 2->16 3->9 5 7 11 13 17 19
+	let num:i32 = 16 * 9 * 5 * 7 * 11 * 13 * 17 * 19;
+	println!("{}", num);
+}
